6bf344d03b7cd2e0f3fcb0c7a31528755cb2e93b
Jhony Freitas <jhony00._@hotmail.com>
2023-02-16 23:19:35 -0300

    Refatoração

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,14 +1,14 @@
-let campos = document.querySelectorAll(".campo");
-let botaoEnviar = document.getElementById("btn-enviar");
+const campos = document.querySelectorAll(".campo");
+const botaoEnviar = document.getElementById("btn-enviar");
 
-campos.forEach(function(campo){
+campos.forEach(campo =>{
     campo.addEventListener('change', function(){
-        const idVerificacaoValido = campo.id;
+        const idVerificacao = campo.id;
         if(campo.value !== ""){
-            document.getElementsByClassName(idVerificacaoValido)[0].classList.add("valido");
-            document.getElementsByClassName(idVerificacaoValido)[0].classList.remove("invalido");
+            document.getElementsByClassName(idVerificacao)[0].classList.add("valido");
+            document.getElementsByClassName(idVerificacao)[0].classList.remove("invalido");
         }else{
-            document.getElementsByClassName(idVerificacaoValido)[0].classList.remove("valido");
+            document.getElementsByClassName(idVerificacao)[0].classList.remove("valido");
         }
     });
 });
@@ -17,6 +17,7 @@ botaoEnviar.addEventListener('click', function(){
     campos.forEach(function(campo){
         const idVerificacaoInvalido = campo.id;
         if(campo.value === ""){
+            campo.classList.add('invalido')
             document.getElementsByClassName(idVerificacaoInvalido)[0].classList.add("invalido");
         };
     });
